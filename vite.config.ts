10aab4846fab5de00bb48e98860269b43983291d
alejandromedina1 <alejandro.medina1@u.icesi.edu.co>
2023-07-08 14:03:04 -0500

    setup project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+const root = resolve(__dirname,'src')
+const outDir = resolve(__dirname, 'dist')
+
+export default defineConfig({
+    root,
+    build:{
+        outDir,
+        emptyOutDir:true,
+        rollupOptions: {
+            input:{
+                main: resolve(root, 'index.html'),
+                "profile": resolve(root, 'profile', 'index.html'),
+            }
+        },
+        target: 'esnext'
+    }
+})
