06b5f5483ff826f512715aa56bb7df47285a7c3f
roritopra <1193102598@u.icesi.edu.co>
2023-07-30 02:03:28 -0500

    [ADD] Agenda profile and styles to the component

--- src/profile/components/components.js
+++ src/profile/components/components.js
@@ -1,12 +1,7 @@
 import "./ButtonDuoIcon/buttonDuoIcon.js"
-
-
 import "./Details/details.js"
-
 import "./LastPost/lastPost.js"
 import "./ChallengeStatus/challengeStatus.js"
-
-
 import "./Table/table.js"
 import "./TurnsCarrousel/turnsCarrousel.js"
 
