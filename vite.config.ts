d4d4442beea96da435a92a16e3b8e6e67a4a0015
alejandromedina1 <alejandro.medina1@u.icesi.edu.co>
2023-08-13 13:55:30 -0500

    simulador VTU terminado

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
                 "cita-profile": resolve(root, 'cita_profile','index.html'),
                 "productos2_profile": resolve(root, 'productos2_profile','index.html'),
                 "productos3_profile": resolve(root, 'productos3_profile','index.html'),
+                "simuladores": resolve(root, 'simuladores','index.html'),
             }
         },
         target: 'esnext'
