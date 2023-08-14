370399f1a19f9d7eeb5748027ff7899780e91200
alejandromedina1 <alejandro.medina1@u.icesi.edu.co>
2023-08-14 11:50:44 -0500

    cdt simulator done

---
+++ src/simulador-credito/simuladores.js
@@ -0,0 +1,13 @@
+document.getElementById("simular").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+document.getElementById('simular').addEventListener('click', () => {
+    document.getElementById('result').style.display="flex"
+    document.getElementById('data').style.display="none"
+})
+
+document.getElementById('back-btn').addEventListener('click', () => {
+    document.getElementById('result').style.display="none"
+    document.getElementById('data').style.display="flex"
+})
