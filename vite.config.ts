188d212fc737d32ed514a79834f2368de6c95ac8
cristianortiz19 <cristclar012@gmail.com>
2023-08-15 00:11:45 -0500

    CDT page

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,8 @@ export default defineConfig({
                 "cita-profile": resolve(root, 'cita_profile','index.html'),
                 "productos2_profile": resolve(root, 'productos2_profile','index.html'),
                 "productos3_profile": resolve(root, 'productos3_profile','index.html'),
-                "ahorradores-inversionistas": resolve(root, 'enviar_giros','index.html'),
+                "enviar-giros": resolve(root, 'enviar_giros','index.html'),
+                "cdt": resolve(root, 'cdt','index.html'),
             }
         },
         target: 'esnext'
