111089625ebe2f0006a4d549e8d42c686003bd66
alejandromedina1 <alejandro.medina1@u.icesi.edu.co>
2023-08-21 21:52:30 -0500

    checkbox fixed

--- src/simulador-ahorro/simuladores.js
+++ src/simulador-ahorro/simuladores.js
@@ -2,22 +2,38 @@ document.getElementById('back-container').addEventListener("click", () => {
     window.location.replace("/simuladores/")
 })
 
-document.getElementById("simular").addEventListener("click", function(event){
+document.getElementById("simular").addEventListener("click", function (event) {
     event.preventDefault()
-  });
+});
 
 document.getElementById('simular').addEventListener('click', () => {
-    document.getElementById('result').style.display="flex"
-    document.getElementById('data').style.display="none"
+    document.getElementById('result').style.display = "flex"
+    document.getElementById('data').style.display = "none"
 })
 
 document.getElementById('back-btn').addEventListener('click', () => {
-    document.getElementById('result').style.display="none"
-    document.getElementById('data').style.display="flex"
+    document.getElementById('result').style.display = "none"
+    document.getElementById('data').style.display = "flex"
 })
 
 
-document.getElementById('checkbox').addEventListener('click', ()=> {
-    console.log(document.getElementById('checkbox').checked)
 
-})
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+console.log(checkboxes)
+const checkbox1 = checkboxes[0];
+const checkbox3 = checkboxes[1];
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function () {
+        if (this.checked) {
+            checkboxes.forEach(otherCheckbox => {
+                if (otherCheckbox !== this) {
+                    otherCheckbox.checked = false;
+                }
+            });
+        }
+
+        
+    });
+});
