455620edcc4fe3cbcf064b1b77531bc3126df9f3
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2023-09-02 11:16:10 -0500

    Started Microcredito page, and fix dropdown issues

---
+++ src/micro-credito/script.js
@@ -0,0 +1,56 @@
+import '../components/bullet-point/index'
+
+const tabButtons = document.querySelectorAll('.tab-button');
+const tabContents = document.querySelectorAll('.tab-content');
+
+const toggleContent = ()=>{
+  if (window.matchMedia('(max-width: 821px)').matches) {
+    tabButtons[0].classList.add('active');
+    tabContents[0].style.display = 'block';
+  
+    tabButtons.forEach(button => {
+      button.addEventListener('click', () => {
+  
+        tabButtons.forEach(btn => btn.classList.remove('active'));
+        tabContents.forEach(content => content.style.display = 'none');
+    
+        button.classList.add('active');
+        const selectedTab = button.getAttribute('data-tab');
+        const selectedContent = document.getElementById(`${selectedTab}-content`);
+        selectedContent.style.display = 'block';
+      });
+    });
+  } else {
+  tabButtons[0].classList.add('active');
+  tabContents[0].style.display = 'grid';
+  
+  tabButtons.forEach(button => {
+    button.addEventListener('click', () => {
+  
+      tabButtons.forEach(btn => btn.classList.remove('active'));
+      tabContents.forEach(content => content.style.display = 'none');
+  
+      button.classList.add('active');
+      const selectedTab = button.getAttribute('data-tab');
+      const selectedContent = document.getElementById(`${selectedTab}-content`);
+      selectedContent.style.display = 'grid';
+    });
+  });
+  }
+}
+
+window.addEventListener('resize', ()=> toggleContent())
+toggleContent();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdowns = document.querySelectorAll(".dropdown");
+
+  dropdowns.forEach(dropdown => {
+    const trigger = dropdown.querySelector(".dropdown-trigger");
+    const content = dropdown.querySelector(".dropdown-tab");
+    console.log(content);
+    trigger.addEventListener("click", () => {
+      content.classList.toggle("show");
+    });
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
                 "ahorradores_inversionistas": resolve(root, 'ahorradores_inversionistas','index.html'),
                 "pqrs": resolve(root, 'pqrs','index.html'),
                 "business-people": resolve(root, 'business-people','index.html'),
+                "micro-credito": resolve(root, 'micro-credito','index.html'),
             }
         },
         target: 'esnext'
