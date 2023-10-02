d01fc4920498118f42f471b843b8ed62cfb9a100
Sunart1 <79371501+Sunart1@users.noreply.github.com>
2023-10-01 19:15:29 -0500

    Continue coding atención al inversionista page

--- src/components/InverCards/InverCards.js
+++ src/components/InverCards/InverCards.js
@@ -2,10 +2,10 @@ const template = document.createElement('template')
 
 template.innerHTML = `
     <link rel="stylesheet" href="../components/InverCards/style.scss">
-    <article class="inver-card">
+    <a href="#" class="inver-link" download><article class="inver-card">
     <img src="/images/inver_card.webp" alt="Patrón de fondo">
     <h3 class="inver-text"></h3>
-    </article>
+    </article></a>
 `
 
 export class InverCard extends HTMLElement{
@@ -17,6 +17,8 @@ export class InverCard extends HTMLElement{
         this.content = this.getAttribute('content')
         this.h3Element = this.shadowRoot.querySelector('h3')
         this.h3Element.innerText = this.content
+        this.aElement = this.shadowRoot.querySelector('.inver-link')
+        this.aElement.download = this.content
     }
 
     static get observedAttributes(){
